3c01979b6ce69d22e18fe38531bc70291e378186
sn3m <33692360+sn3m@users.noreply.github.com>
2020-11-27 17:33:13 +0100

    Add platform generation

--- Platform.js
+++ Platform.js
@@ -1,9 +1,17 @@
 import Node from './Node.js';
+import Utils from './Utils.js';
 
 export default class Platform extends Node {
-    constructor(mesh, image, options) {
+    constructor(id, mesh, image, options) {
         super(options);
+        Utils.init(this, this.constructor.defaults, options);
+
+        this.id = id;
         this.mesh = mesh;
         this.image = image;
     }
 }
+
+Platform.defaults = {
+    velocity: [0, 0, 0],
+};
